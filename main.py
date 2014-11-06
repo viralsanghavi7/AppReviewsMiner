511ca6a645663d8038b1167c53f684cca4f769c3
unknown <vjsangha@ncsu.edu>
2014-11-06 01:26:46 -0500

    tagging of words

--- main.py
+++ main.py
@@ -21,6 +21,13 @@ compatibility_total = 0
 request_total = 0
 general_total = 0
 
+performance_tags = []
+userinterface_tags = []
+compatibility_tags = []
+request_tags = []
+general_tags = []
+hit_words_tags = []
+
 
 performance_final_list = []
 userinterface_final_list = []
@@ -52,7 +59,7 @@ for x in tokenized:
     imp_words.append([word for word in x if word.lower()
                       not in stopwords and word.isalpha()])
 
-#print(imp_words)
+print(imp_words)
 
 
 
@@ -60,13 +67,14 @@ for x in tokenized:
 
 review_line_count=0
 for review_line in imp_words:
-    print(review_line)
+    #print(review_line)
 
     criteria = xlrd.open_workbook('Review_Criteria.xlsx',on_demand=True)
     for name in criteria.sheet_names():
         category_total_score = 0
         sheet = criteria.sheet_by_name(name)
         request_count =0
+        hit_words_tags = []
         for [category,score] in zip(sheet.col(0),sheet.col(1)):
             in_string = category.value
 
@@ -76,33 +84,40 @@ for review_line in imp_words:
                         if (name == request and request_count == 0):
                             if in_string in " ".join(review_line):
                                 request_count += 1
+                                hit_words_tags.append(in_string)
                                 category_total_score += score.value
                         else:
                             string_length = len(in_string)
                             if in_string in review_word and in_string[0:string_length] == review_word[0:string_length]:
+                                hit_words_tags.append(review_word)
                                 category_total_score += score.value
 
         if(name == performance):
                 performance_total = category_total_score
+                performance_tags = hit_words_tags
         elif(name == userinterface):
                 userinterface_total = category_total_score
+                userinterface_tags = hit_words_tags
         elif(name == compatibility):
                 compatibility_total = category_total_score
+                compatibility_tags = hit_words_tags
         elif(name == request):
                 request_total = category_total_score
+                request_tags = hit_words_tags
         elif(name == general):
                 general_total = category_total_score
+                general_tags = hit_words_tags
 
     if performance_total != 0:
-        performance_final_list.append([all_reviews[review_line_count],performance,performance_total])
+        performance_final_list.append([all_reviews[review_line_count],performance,performance_tags,performance_total])
     if compatibility_total != 0:
-        compatibility_final_list.append([all_reviews[review_line_count],compatibility,compatibility_total])
+        compatibility_final_list.append([all_reviews[review_line_count],compatibility,compatibility_tags,compatibility_total])
     if userinterface_total != 0:
-        userinterface_final_list.append([all_reviews[review_line_count],userinterface,userinterface_total])
+        userinterface_final_list.append([all_reviews[review_line_count],userinterface,userinterface_tags,userinterface_total])
     if request_total != 0:
-        request_final_list.append([all_reviews[review_line_count],request,request_total])
+        request_final_list.append([all_reviews[review_line_count],request,request_tags,request_total])
     if general_total != 0:
-        general_final_list.append([all_reviews[review_line_count],general,general_total])
+        general_final_list.append([all_reviews[review_line_count],general,general_tags,general_total])
     review_line_count += 1
 
 performance_final_list.sort(key=lambda x: x[2])
@@ -124,6 +139,8 @@ for member in request_final_list:
 general_final_list.sort(key=lambda x: x[2])
 for member in general_final_list:
     print(member)
+
+
 
 
 # file1 = open('criteria.csv')
